a2356e19919d8d916f417d4c0814e0dd7f759267
Katie <kxh814@cs.bham.ac.uk>
2019-11-15 17:19:07 +0000

    Changed to satisfy creating an xml tree using new internal tree structure, pres only

--- xmlout.py
+++ xmlout.py
@@ -9,31 +9,39 @@ def xmlout(tree):
              should create a file if one with that name doesn't exist, then write'''
     '''next: want to walk the tree and make my personal structure back into and etree'''
     
-    root = etree.Element(tree.get_name())
-    makechildren(tree, root)
+    # root = etree.Element(tree.get_name())
+    # makechildren(tree, root)
+    root = etree.Element("math")
+    mrow = etree.SubElement(root, "mrow")
+
+    #make the other nodes from the internal tree structure
+
+    tree.outputpresxml(mrow)
+    
+    
     # child = tree.get_child()
     # root.append(etree.Element(child.get_name()))
 
     return etree.tostring(root)
 
-def makechildren(treenodeparent, xmlparent):
-    #print(treenodeparent)
-
-    child = treenodeparent.get_child()
-    while child != None:
-        # xmlparent.append(etree.Element(child.get_name()))
-        # parent = xmlparent.append(etree.Element(child.get_name()))
-        print(child.get_name())
-        try:
-            parent = etree.SubElement(xmlparent, child.get_name())
-            print(parent)
-            makechildren(child, parent)
-        except ValueError:
-            #parent = etree.SubElement(xmlparent, "OPERATOR")
-            '''it didn't like making operators into tags. 
-            also strip required for say, x'''
-            xmlparent.text = child.get_name()#.strip()
-
-
-        child = child.get_sibling()
+# def makechildren(treenodeparent, xmlparent):
+#     #print(treenodeparent)
+# 
+#     child = treenodeparent.get_child()
+#     while child != None:
+#         # xmlparent.append(etree.Element(child.get_name()))
+#         # parent = xmlparent.append(etree.Element(child.get_name()))
+#         print(child.get_name())
+#         try:
+#             parent = etree.SubElement(xmlparent, child.get_name())
+#             print(parent)
+#             makechildren(child, parent)
+#         except ValueError:
+#             #parent = etree.SubElement(xmlparent, "OPERATOR")
+#             '''it didn't like making operators into tags. 
+#             also strip required for say, x'''
+#             xmlparent.text = child.get_name()#.strip()
+# 
+# 
+#         child = child.get_sibling()
 
