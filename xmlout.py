5d85ecb04105dd88d093158c3f031a44fb6007ac
Katie <kxh814@cs.bham.ac.uk>
2019-11-23 17:15:57 +0000

    Added write to file method

--- xmlout.py
+++ xmlout.py
@@ -17,10 +17,8 @@ def presxmlout(tree):
     #make the other nodes from the internal tree structure
 
     tree.outputpresxml(mrow)
-    
-    
-    # child = tree.get_child()
-    # root.append(etree.Element(child.get_name()))
+
+    write_to_file(str(etree.tostring(root)))
 
     return etree.tostring(root)
 
@@ -34,13 +32,11 @@ def contxmlout(tree):
 
     root = etree.Element("math")
 
-
     # make the other nodes from the internal tree structure
 
     tree.outputcontxml(root)
 
-    # child = tree.get_child()
-    # root.append(etree.Element(child.get_name()))
+    write_to_file(str(etree.tostring(root)))
 
     return etree.tostring(root)
 
@@ -56,8 +52,24 @@ def translatename(nodename):
         newname = "times"
     if nodename == "/":
         newname = "divide"
-
+    if nodename == "!":
+        newname = "factorial"
+    if nodename == "sqrt":
+        newname = "root"
+    remain = ["power", "sin", "cos", "tan"]
+    if nodename in remain:
+        newname = nodename
     return newname
+
+def write_to_file(treestring):
+    with open("output.xml", "w") as output_file:
+        output_file.write("<!DOCTYPE math PUBLIC "+ "\"-//W3C//DTD MathML 3.0//EN\"" +" \"http://www.w3.org/Math/DTD/mathml3/mathml3.dtd\"" +"> \n")
+    with open("output.xml", 'a') as output_file:
+        output_file.write(treestring[2:-1])
+    '''above: if the file exists it writes to (and over) that file
+             should create a file if one with that name doesn't exist, then write'''
+    '''next: want to walk the tree and make my personal structure back into and etree'''
+
 
 # def makechildren(treenodeparent, xmlparent):
 #     #print(treenodeparent)
